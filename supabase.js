118a619efb3213cf6823ca20eb1d67205348e82d
vietuj62-maker <vietuj62@gmail.com>
2026-03-13 01:55:20 +0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,1 +1,7 @@
 
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+const supabaseUrl = "SUPABASE_URL"
+const supabaseKey = "SUPABASE_PUBLIC_KEY"
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
