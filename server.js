53914ae9a834ae90d35606f8e02d560a29884be1
vietuj62-maker <vietuj62@gmail.com>
2026-03-13 04:29:12 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,3 +4,15 @@ let content = req.body.content
 let amount = req.body.amount
 
 })
+const express = require("express")
+const app = express()
+
+app.use(express.json())
+
+app.post("/webhook-bank",(req,res)=>{
+
+console.log(req.body)
+
+})
+
+app.listen(3000)
